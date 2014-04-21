907015f3438cf51423eaa01ca8b7e5365f9e78f4
sharanya <sharans@thoughtworks.com>
2014-04-21 18:11:51 +0530

    Send info on from server on bus lookup

--- app/controllers/bus_controller.rb
+++ app/controllers/bus_controller.rb
@@ -1,7 +1,7 @@
 class BusController < ApplicationController
   ROUTES = {
       "M15" => ['mylapore', 'adayar', 'velachery', 'pallikaranai', 'medavakkam'],
-      "219A" => ['ambattur', 'golden flats', 'anna nagar', 't nagar', 'tidel']
+      "219A" => ['Ambattur', 'Collector Nagar', 'Thirumangalam', 't nagar', 'tidel']
   }
   DIRECTIONS = ["ONWARD", "RETURN"]
 
@@ -11,15 +11,23 @@ class BusController < ApplicationController
   end
 
   def locate
-    bus_no = params[:bus_number]
+    bus_number = params[:bus_number]
     source = params[:source]
-    dest = params[:dest]
-    direction =  find_direction(bus_no, source, dest)
-
-    head :ok
+    dest = params[:dest] #currently unused - will be used for direction computation
+    #direction =  find_direction(bus_no, source, dest)
+    render json: buses_along(bus_number, source) #assuming direction is onward - can compute direction later.
   end
 
   private
+  def buses_along(bus_number, source)
+    starting_points = starting_points(bus_number, source)
+    BusLocation.where(bus_number: bus_number, source: starting_points)
+  end
+
+  def starting_points(bus_number, source)
+    ROUTES[bus_number].slice(0, ROUTES[bus_number].index(source))
+  end
+
   def find_direction(bus_no, source, dest)
     route = ROUTES[bus_no]
     route.index(source) < route.index(dest) ? "ONWARD" : "RETURN"
--- app/models/bus_location.rb
+++ app/models/bus_location.rb
@@ -1,2 +1,13 @@
 class BusLocation < ActiveRecord::Base
+  before_create :set_created_time
+  before_save :set_updated_time
+
+  def set_created_time
+    created_at = Time.now
+  end
+
+  def set_updated_time
+    updated_at = Time.now
+  end
+
 end
